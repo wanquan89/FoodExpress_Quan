0bf7839258ec512d866078fb2073a96b6e33afce
wanquan89 <wanquan89@hotmail.com>
2015-10-13 15:12:43 -0400

    Update connect.php

--- core/database/connect.php
+++ core/database/connect.php
@@ -1,7 +1,7 @@
 <?php
 $connect_error='Sorry, we\'re experiencing connection problems.';
-mysql_connect("localhost", "ashamsad","hivesql") or die($connect_error);
-mysql_select_db("biomuta") or die($connect_error);
+mysql_connect("localhost", "username","password") or die($connect_error);
+mysql_select_db("database") or die($connect_error);
 
 
 ?>
